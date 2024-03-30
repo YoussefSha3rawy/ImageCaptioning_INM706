Removed Luong attention Separated encoder and decoder learning rates Fixed bug in bleu score calculation Reverted pad token change Fixed bug of dropout not bein disabled in eval Added dropout and multihead attention to encoder

dataset.py
@@ -20,15 +20,13 @@ if torch.backends.mps.is_available():
 
 
 class Lang:
-    PAD_TOKEN = 0
-    SOS_TOKEN = 1
-    EOS_TOKEN = 2
+    EOS_TOKEN = 1
 
     def __init__(self, name):
         self.name = name
         self.word2index = {}
         self.word2count = {}
-        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS"}
+        self.index2word = {0: "SOS", 1: "EOS"}
         self.n_words = 2  # Count SOS and EOS
 
     def addSentence(self, sentence):
@@ -125,8 +123,7 @@ class ImageCaptioningDataset(Dataset):
         tokenized_sentence = [self.lang.word2index[word]
                               for word in sentence.split(' ')][:self.max_length - 1]
         tokenized_sentence.append(self.lang.EOS_TOKEN)
-        input_ids = torch.full(
-            (self.max_length,), self.lang.PAD_TOKEN, dtype=torch.long)
+        input_ids = torch.zeros(self.max_length, dtype=torch.long)
         input_ids[:len(tokenized_sentence)] = torch.tensor(tokenized_sentence)
         return input_ids, len(tokenized_sentence)
 

main.py
@@ -24,6 +24,8 @@ print(f'{device = }')
 
 
 def evaluate(encoder, decoder, dataloader):
+    encoder.eval()
+    decoder.eval()
     all_ground_truths_captions = []
     decoded_sentences = []
     with torch.no_grad():
@@ -63,13 +65,12 @@ def evaluate(encoder, decoder, dataloader):
 
 
 def calculate_bleu_scores(candidate_corpus, reference_corpus, max_n=4):
-    weights = [1]
     bleu_scores = []
     for n in range(1, max_n+1):
+        weights = [1/n] * n
         bleu = bleu_score(candidate_corpus,
                           reference_corpus, max_n=n, weights=weights)
         bleu_scores.append(bleu)
-        weights.insert(0, 0)
 
     return bleu_scores
 
@@ -118,6 +119,8 @@ def plot_and_show_attention(encoder, decoder, input_sentence, input_tensor, outp
 
 def train_epoch(dataloader, encoder, decoder, encoder_optimizer,
                 decoder_optimizer, criterion, teacher_forcing_ratio):
+    encoder.train()
+    decoder.train()
 
     total_loss = 0
     for i, (image_name, image_tensor, tokenized_captions, caption_texts) in enumerate(dataloader):
@@ -143,21 +146,22 @@ def train_epoch(dataloader, encoder, decoder, encoder_optimizer,
             loss.backward()
 
             total_loss += loss.item()
-        decoder_optimizer.step()
-        encoder_optimizer.step()
+            decoder_optimizer.step()
+            encoder_optimizer.step()
         print(f'Step {i}/{len(dataloader)}', end='\r')
     return total_loss / len(dataloader)
 
 
-def train(
-        train_dataloader, test_dataloader, encoder, decoder, logger, n_epochs, teacher_forcing_ratio=1.0, learning_rate=0.001, early_stopping=np.inf,
-        print_every=100, plot_every=100):
+def train(train_dataloader, test_dataloader, encoder, decoder, logger, n_epochs, teacher_forcing_ratio=1.0,
+          encoder_learning_rate=0.001, decoder_learning_rate=0.001, early_stopping=np.inf, print_every=100, plot_every=100):
     plot_losses = []
     print_loss_total = 0  # Reset every print_every
     plot_loss_total = 0  # Reset every plot_every
 
-    encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
-    decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
+    encoder_optimizer = optim.Adam(
+        encoder.parameters(), lr=encoder_learning_rate)
+    decoder_optimizer = optim.Adam(
+        decoder.parameters(), lr=decoder_learning_rate)
     criterion = nn.NLLLoss()
 
     max_bleu = 0

models.py
@@ -28,16 +28,16 @@ class ImageEncoderFC(nn.Module):
 
 
 class ImageEncoderAttention(nn.Module):
-    def __init__(self, hidden_size: int, freeze_backbone=False, nr_heads=1, backbone_name: str = None):
+    def __init__(self, hidden_size: int, freeze_backbone=False, backbone_name: str = None, dropout_p=0.2, nr_heads=0):
         super(ImageEncoderAttention, self).__init__()
         self.hidden_size = hidden_size
+        self.nr_heads = nr_heads
 
-        backbone = resnet50(weights=ResNet50_Weights.DEFAULT)
+        backbone = resnet101(weights=ResNet101_Weights.DEFAULT)
 
         self.cnn = nn.Sequential(*list(backbone.children())[:-2])
 
-        # self.attention = AttentionMultiHead(
-        #     input_size=2048, out_size=hidden_size, nr_heads=nr_heads)
+        self.dropout = nn.Dropout(dropout_p)
 
         if hasattr(backbone, 'fc'):
             self.out_features = backbone.fc.in_features
@@ -48,27 +48,33 @@ class ImageEncoderAttention(nn.Module):
             for param in self.cnn.parameters():
                 param.requires_grad = False
 
-        # self.cnn.fc = nn.Linear(self.cnn.fc.in_features, hidden_size)
-        # self.conv = nn.Conv2d(2048, 20, kernel_size=1)
+        if nr_heads > 0:
+            self.self_attention = AttentionMultiHead(
+                input_size=self.out_features, out_size=self.out_features, nr_heads=nr_heads)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.cnn(x)
-
+        if self.nr_heads > 0:
+            x = x.permute(0, 2, 3, 1)
+            batch, i, j, features = x.size()
+            x = x.view(batch, -1, features)
+            x, _ = self.self_attention(x)
+            x = x.view(batch, i, j, features)
+            x = x.permute(0, 3, 1, 2)
+        x = self.dropout(x)
         return x
 
 
 class AttentionTypes(Enum):
     NONE = 'None'
     BAHDANAU = 'Bahdanau'
-    LUONG = 'Luong'
 
 
 class DecoderWithAttention(nn.Module):
-    PAD_token = 0
-    SOS_token = 1
-    EOS_token = 2
+    SOS_token = 0
+    EOS_token = 1
 
-    def __init__(self, embedding_size, hidden_size, encoder_dim, output_size, max_length=10, beam_size=1, attention_type=AttentionTypes.NONE, dropout_p=0.1, device=torch.device('cpu')):
+    def __init__(self, embedding_size, hidden_size, encoder_dim, output_size, max_length=10, attention_type=AttentionTypes.NONE, dropout_p=0.1, device=torch.device('cpu')):
         super(DecoderWithAttention, self).__init__()
         self.embedding = nn.Embedding(output_size, embedding_size)
         self.hidden_size = hidden_size
@@ -78,17 +84,14 @@ class DecoderWithAttention(nn.Module):
         self.attention_type = attention_type
         self.encoder_dim = encoder_dim
 
-        print(attention_type)
+        assert attention_type in [
+            member.value for member in AttentionTypes], 'Attention type not supported'
+
         if attention_type == AttentionTypes.BAHDANAU.value:
             self.attention_module = BahdanauAttention(
                 hidden_size, self.encoder_dim)
             self.attention_function = self.forward_step_bahdanau
             self.gru_input_size = embedding_size + self.encoder_dim
-        elif attention_type == AttentionTypes.LUONG.value:
-            self.attention = LuongAttn('general', hidden_size)
-            self.attention_function = self.forward_step_luong
-            self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
-            self.concat = nn.Linear(hidden_size * 2, hidden_size)
         else:
             self.gru_input_size = embedding_size
         self.gru = nn.GRU(self.gru_input_size, hidden_size, batch_first=True)
@@ -113,7 +116,7 @@ class DecoderWithAttention(nn.Module):
         attentions = []
 
         for i in range(self.max_length):
-            if self.attention_type == AttentionTypes.NONE:
+            if self.attention_type == AttentionTypes.NONE.value:
                 decoder_output, decoder_hidden = self.forward_step_no_attention(
                     decoder_input, decoder_hidden
                 )
@@ -143,7 +146,7 @@ class DecoderWithAttention(nn.Module):
         output = self.embedding(input)
         output = self.dropout(output)
         output, hidden = self.gru(output, hidden)
-        output = self.out(output)
+        output = self.out(self.dropout(output))
         return output, hidden
 
     def forward_step_bahdanau(self, input, hidden, encoder_outputs):
@@ -152,29 +155,13 @@ class DecoderWithAttention(nn.Module):
         context, attn_weights = self.attention_module(query, encoder_outputs)
         input_gru = torch.cat((embedded, context), dim=2)
         output, hidden = self.gru(input_gru, hidden)
-        output = self.out(output)
+        output = self.out(self.dropout(output))
         return output, hidden, attn_weights
-
-    def forward_step_luong(self, input, hidden, encoder_outputs):
-        embedded = self.dropout(self.embedding(input))
-        output, hidden = self.gru(embedded, hidden)
-        attn_weights, _ = self.attention(output, encoder_outputs)
-        context = attn_weights.bmm(encoder_outputs)
-
-        rnn_output = output.squeeze(1)
-        context = context.squeeze(1)
-        concat_input = torch.cat((rnn_output, context), 1)
-        concat_output = torch.tanh(self.concat(concat_input))
-        output = self.out(concat_output)
-        output = F.softmax(output, dim=1)
-        # Return output and final hidden state
-        return output.unsqueeze(1), hidden, None
 
 
 class SelfAttnDecoderRNN(nn.Module):
-    PAD_token = 0
-    SOS_token = 1
-    EOS_token = 2
+    SOS_token = 0
+    EOS_token = 1
 
     def __init__(self, embedding_size, hidden_size, output_size, max_length=10, device=torch.device('cpu')):
         super(SelfAttnDecoderRNN, self).__init__()